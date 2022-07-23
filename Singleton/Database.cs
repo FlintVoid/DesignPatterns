5653fa345d1ce01c0032e3bbf849d8fcb114bd52
Petr Kononov <p.kononov@sbergames.ru>
2022-07-24 02:18:26 +0300

    singleton

---
+++ Singleton/Database.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public class Database
+    {
+        private static Database _instance;
+
+        private Stack<string> _entities;
+        
+        public static Database GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Database();
+            }
+
+            return _instance;
+        }
+        
+        public IEnumerable<string> SelectALL()
+        {
+            return _entities;
+        }
+
+        public void RemoveLast()
+        {
+            _entities.Pop();
+        }
+
+        private Database()
+        {
+            _entities = new Stack<string>();
+            _entities.Push("c#");
+            _entities.Push("java");
+            _entities.Push("kotlin");
+            _entities.Push("phyton");
+            _entities.Push("js");
+            _entities.Push("prolog");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Database database = Database.GetInstance();
+            ShowAll(database.SelectALL());
+            database.RemoveLast();
+            
+            ShowAll(Database.GetInstance().SelectALL());
+        }
+
+        private static void ShowAll(IEnumerable<string> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Console.Write(entity + ' ');
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
